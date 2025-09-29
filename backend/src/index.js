96c420e5332b6a8de6c5a976c72dcba64f6e10ad
theNkds <thebiggestdevelopper@gmail.com>
2025-09-29 19:23:30 +0100

    run

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     // origin: 'http://localhost:5173',
-    origin: 'http://localhost:5173',
+    origin: 'https://realtime-chatting-app-nine.vercel.app',
     credentials: true,
 }));
 
