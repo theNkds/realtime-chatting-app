8202000f14a16de69951c0420734d2836b0f7d92
theNkds <thebiggestdevelopper@gmail.com>
2025-09-29 20:50:51 +0100

    resolution des bugs sockets sur le client et serveur

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -9,6 +9,8 @@ const io = new Server(server, {
     cors: {
         // origin: ["http://localhost:5173"],
         origin: ["https://realtime-chatting-app-nine.vercel.app"],
+        methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+        credentials: true
     }
 });
 
