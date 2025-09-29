3955be81f7cf08c6f95072b560532e20765b8fcf
theNkds <thebiggestdevelopper@gmail.com>
2025-09-29 19:53:02 +0100

    next chance

--- backend/src/index.js
+++ backend/src/index.js
@@ -33,6 +33,6 @@ app.use("/api/messages", messageRoutes);
 // }
 
 server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`); 
     connectDB();
+    console.log(`Server is running on port ${PORT}`); 
 });
